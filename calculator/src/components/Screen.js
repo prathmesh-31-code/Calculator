448f6e7e5c0b00055c42feeebfeaf4ff0934e7a5
Prathmesh Balsurkar <prathmeshb.code@gmail.com>
2024-09-18 16:08:04 +0530

    Intial commit

---
+++ calculator/src/components/Screen.js
@@ -0,0 +1,14 @@
+
+import "./Screen.css";
+
+import React from 'react';
+
+const Screen = ({ value }) => {
+  return (
+    <div className="screen">
+      {value}
+    </div>
+  );
+};
+
+export default Screen;
